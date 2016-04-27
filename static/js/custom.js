63a3f6af3fa29c4497ecb54903b171d094eb1d3c
Philipp Klaus <philipp.klaus@gmail.com>
2016-04-27 14:21:28 +0200

    moving the search.js to custom.js

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,4 +1,5 @@
 
+/* start search upon pressing [Enter] */
 $(document).ready(function() {
     $('#search-form').submit( function() {
         //window.location = '/search/' + $('input[name="toggle"]:checked').val();          
